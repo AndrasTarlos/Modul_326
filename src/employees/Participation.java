93bd03fc317ebcbe8cbe90ed21580ebb30a6fbed
AndrasTarlos <tarlosa@bzz.ch>
2022-06-01 14:18:13 +0200

    implemented Participation.java

--- src/employees/Participation.java
+++ src/employees/Participation.java
@@ -1,5 +1,46 @@
 package employees;
 
-public class Participation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Participation {
+    private List<JobFunctions> function = new ArrayList<>();
+    private List<Team> teams = new ArrayList<>();
+    private Person owner;
+
+    public Participation(Person person) {
+
+    }
+
+    public void addFunction(JobFunction jobFunction) {
+
+    }
+
+    public String getFunctionName(int index) {
+        return function.get(index);
+    }
+
+    public void removeFunction(int index) {
+        function.remove();
+    }
+
+    public int getNumberOfFunctions() {
+        return function.getSize();
+    }
+
+    public void addTeam(Teams teams) {
+        teams.add(teams);
+    }
+
+    public String getTeamName(int index) {
+        return "";
+    }
+
+    public void removeTeam(int index) {
+
+    }
+
+    public int getNumberOfTeams() {
+        return 1;
+    }
 }
