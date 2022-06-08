bda3273d14bb620ce40469499e5399f344fae6ef
francescoryu <francescoryu04@gmail.com>
2022-06-08 20:46:36 +0200

    edited assignment

--- src/main/java/gui/Assignment.java
+++ src/main/java/gui/Assignment.java
@@ -1,10 +1,19 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Assignment extends JPanel {
 
-    public Assignment() {
+    JPanel personListPanel;
+    JList personList;
 
+
+    public Assignment() {
+        personListPanel = new JPanel();
+        personList = new JList();
+
+        this.add(personListPanel, BorderLayout.WEST);
+        this.setVisible(true);
     }
 }
