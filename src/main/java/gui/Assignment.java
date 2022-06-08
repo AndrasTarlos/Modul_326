42823c9f40df6facde189f9bbc56dcf6308a8d88
francescoryu <francescoryu04@gmail.com>
2022-06-08 20:53:30 +0200

    edited assignment

--- src/main/java/gui/Assignment.java
+++ src/main/java/gui/Assignment.java
@@ -2,17 +2,20 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Assignment extends JPanel {
 
     JPanel personListPanel;
     JList personList;
-
+    String[] person = {"jklsg", "gjulgjdlfkgjdfkg", "jgkjslgjskfaghga", "riiririr", "jjgkfgfkg"};
 
     public Assignment() {
         personListPanel = new JPanel();
-        personList = new JList();
+        personList = new JList(person);
 
+
+        personListPanel.add(personList);
         this.add(personListPanel, BorderLayout.WEST);
         this.setVisible(true);
     }
