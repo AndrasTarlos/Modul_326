42b59b6d9551a5517fbe45a3e8341f6f592a2d00
francescoryu <francescoryu04@gmail.com>
2022-06-09 22:46:29 +0200

    edited Assignment.java

--- src/main/java/gui/Assignment.java
+++ src/main/java/gui/Assignment.java
@@ -1,31 +1,39 @@
 package gui;
 
-import employees.Person;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Assignment extends JPanel {
 
+    JPanel personEditPanel;
     JPanel personListPanel;
+
     DefaultListModel personListModel;
     JList personList;
+    JLabel overviewLabel;
 
     public Assignment() {
         this.setLayout(new BorderLayout());
-        personListPanel = new JPanel();
-        personListModel = new DefaultListModel();
+        personEditPanel = new JPanel();
+        personEditPanel.setLayout(new BorderLayout());
+        personEditPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
+        personListPanel = new JPanel();
+        personListPanel.setLayout(new BorderLayout());
+
+        overviewLabel = new JLabel("Übersicht:");
+
+        personListModel = new DefaultListModel();
         personListModel.addElement("Francesco Ryu");
         personListModel.addElement("Andras Tarlos");
         personListModel.addElement("Julian Janik");
 
         personList = new JList(personListModel);
-
+        personEditPanel.add(personListPanel, BorderLayout.CENTER);
+        personEditPanel.add(overviewLabel, BorderLayout.NORTH);
         personListPanel.add(personList);
 
-        this.add(personListPanel, BorderLayout.WEST);
+        this.add(personEditPanel, BorderLayout.WEST);
         this.setVisible(true);
     }
 }
