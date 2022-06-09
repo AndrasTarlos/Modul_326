e5fa391749f554e64a3dc7482d1b8523696a402b
francescoryu <francescoryu04@gmail.com>
2022-06-09 22:01:17 +0200

    changed assignment gui structure to DefaultListModel

--- src/main/java/gui/Assignment.java
+++ src/main/java/gui/Assignment.java
@@ -1,5 +1,7 @@
 package gui;
 
+import employees.Person;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -7,15 +9,22 @@ import java.util.ArrayList;
 public class Assignment extends JPanel {
 
     JPanel personListPanel;
+    DefaultListModel personListModel;
     JList personList;
-    String[] person = {"jklsg", "gjulgjdlfkgjdfkg", "jgkjslgjskfaghga", "riiririr", "jjgkfgfkg"};
 
     public Assignment() {
+        this.setLayout(new BorderLayout());
         personListPanel = new JPanel();
-        personList = new JList(person);
+        personListModel = new DefaultListModel();
 
+        personListModel.addElement("Francesco Ryu");
+        personListModel.addElement("Andras Tarlos");
+        personListModel.addElement("Julian Janik");
+
+        personList = new JList(personListModel);
 
         personListPanel.add(personList);
+
         this.add(personListPanel, BorderLayout.WEST);
         this.setVisible(true);
     }
