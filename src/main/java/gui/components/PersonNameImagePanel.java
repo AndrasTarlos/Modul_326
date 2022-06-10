3cdeabe39da5c2a2a161935b6b5493ff4ead3f92
Francesco Ryu <francescoryu04@gmail.com>
2022-06-10 23:39:00 +0200

    Added placeholders

--- src/main/java/gui/components/PersonNameImagePanel.java
+++ src/main/java/gui/components/PersonNameImagePanel.java
@@ -1,20 +1,28 @@
 package gui.components;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
 public class PersonNameImagePanel extends JPanel {
     JTextField nameInputTextField;
     JLabel personNameLabel;
 
+    JLabel imagePlaceHolder;
+
     public PersonNameImagePanel() {
         personNameLabel = new JLabel("  Name: ");
+        ImageIcon picture = new ImageIcon("img.png");
 
-        nameInputTextField = new JTextField();
+        nameInputTextField = new JTextField("PLACEHOLDER");
         nameInputTextField.setColumns(25);
+        nameInputTextField.setEditable(false);
 
-        /*BufferedImage myPicture = ImageIO.read(new File("img.png"));
-        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-        add(picLabel);*/
+        imagePlaceHolder = new JLabel();
+        imagePlaceHolder.setPreferredSize(new Dimension( 200, 200));
+        imagePlaceHolder.setBorder(new TitledBorder("PLACEHOLDER"));
 
         this.add(personNameLabel);
         this.add(nameInputTextField);
+        this.add(imagePlaceHolder);
     }
 }
