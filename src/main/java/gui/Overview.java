6b98235c5e36b046fe78bce4ec8a6207b11bc96e
francescoryu <francescoryu04@gmail.com>
2022-06-12 01:00:29 +0200

    LOL

--- src/main/java/gui/Overview.java
+++ src/main/java/gui/Overview.java
@@ -1,13 +1,29 @@
 package gui;
 
+import gui.components.AddAssignmentPanel;
+import gui.components.PersonInfoPanel;
+import gui.components.PersonOverview;
+
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 
 public class Overview extends JPanel {
+    PersonInfoPanel personInfoPanel;
+    AddAssignmentPanel personAssignmentPanel;
+    JPanel personenPanel;
+    PersonOverview personOverview;
     public Overview() {
-        setVisible(true);
-        setSize(500, 500);
-        JButton test = new JButton("EfEFEFE");
-        this.add(test);
+        this.setLayout(new BorderLayout());
+        personenPanel = new JPanel(new BorderLayout());
+        personInfoPanel = new PersonInfoPanel();
+        personenPanel.setBorder(new TitledBorder("  Personen:    "));
+
+        personOverview = new PersonOverview(personInfoPanel, personAssignmentPanel);
+        personOverview.setPreferredSize(new Dimension(170, 0));
+        personenPanel.add(personOverview, BorderLayout.WEST);
+        personenPanel.add(personInfoPanel, BorderLayout.CENTER);
+        this.add(personenPanel);
 
     }
 }
