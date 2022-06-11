9ed6af49ae9afdc86687ccade3c6e0ca4f8fc984
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-06-11 13:33:05 +0200

    Changed main.java

--- src/main/java/utils/Main.java
+++ src/main/java/utils/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ReadWriteJSON readWriteJSON = new ReadWriteJSON();
         Company company = readWriteJSON.readCompanyJSON();
-        readWriteJSON.writeCompanyJSON(company);
+        //readWriteJSON.writeCompanyJSON(company);
     }
 }
