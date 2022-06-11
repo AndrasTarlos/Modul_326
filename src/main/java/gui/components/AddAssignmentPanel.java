bacc397fa245e350921bff36c8e692d7e47bdd69
francescoryu <francescoryu04@gmail.com>
2022-06-11 21:10:43 +0200

    Edited some classes

--- src/main/java/gui/components/AddAssignmentPanel.java
+++ src/main/java/gui/components/AddAssignmentPanel.java
@@ -15,6 +15,9 @@ public class AddAssignmentPanel extends JPanel {
 
     JTextField abteilungTextField;
 
+    JComboBox<String> funktionComboBox;
+    JList selectFunktionList;
+
     public AddAssignmentPanel() {
         this.setBorder(new MatteBorder(2, 0, 0, 0, Color.BLACK));
         this.setLayout(new BorderLayout());
@@ -22,41 +25,27 @@ public class AddAssignmentPanel extends JPanel {
         labelPanel = new JPanel();
         selectionPanel = new JPanel();
 
-        labelPanel.setLayout(new BorderLayout());
-        selectionPanel.setLayout(new BorderLayout());
-
-        abteilung = new JLabel("Abteilung:  ");
-        funktion = new JLabel("Funktion:    ");
-        teams = new JLabel("Teams:  ");
-
-        Border borderAbteilung = abteilung.getBorder();
-        Border marginAbteilung = new EmptyBorder(50, 10, 0, 0);
-        abteilung.setBorder(new CompoundBorder(borderAbteilung, marginAbteilung));
-
-        Border borderFunktion = funktion.getBorder();
-        Border marginFunktion = new EmptyBorder(0, 10, 0, 0);
-        funktion.setBorder(new CompoundBorder(borderFunktion, marginFunktion));
-
-        Border borderTeams = teams.getBorder();
-        Border marginTeams = new EmptyBorder(0, 10, 50, 0);
-        teams.setBorder(new CompoundBorder(borderTeams, marginTeams));
+        abteilung = new JLabel("Abteilung:");
+        funktion = new JLabel("Funktion:");
+        teams = new JLabel("Teams:");
 
         abteilungTextField = new JTextField();
         abteilungTextField.setColumns(20);
 
-        Border borderAbteilungTextField = abteilungTextField.getBorder();
-        abteilungTextField.setBorder(new CompoundBorder(borderAbteilungTextField, marginAbteilung));
+        funktionComboBox = new JComboBox<>();
+        funktionComboBox.setPreferredSize(new Dimension(50, 20));
+
+        selectFunktionList = new JList();
 
         selectionPanel.add(abteilungTextField);
+        selectionPanel.add(funktionComboBox);
 
-        labelPanel.add(abteilung, BorderLayout.NORTH);
-        labelPanel.add(funktion, BorderLayout.CENTER);
-        labelPanel.add(teams, BorderLayout.SOUTH);
+        labelPanel.add(abteilung);
+        labelPanel.add(funktion);
+        labelPanel.add(teams);
 
         labelPanel.setBorder(new TitledBorder(""));
         labelPanel.setPreferredSize(new Dimension(100, 0));
-
-        selectionPanel.add(abteilungTextField, BorderLayout.NORTH);
 
         this.add(labelPanel, BorderLayout.WEST);
         this.add(selectionPanel, BorderLayout.CENTER);
