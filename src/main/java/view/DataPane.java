471334d0fbd2a1d325ada461734543a0e188e890
AndrasTarlos <76171862+AndrasTarlos@users.noreply.github.com>
2022-06-19 10:49:58 +0200

    Changes by franc

--- src/main/java/view/DataPane.java
+++ src/main/java/view/DataPane.java
@@ -1,7 +1,18 @@
 package view;
 
+import view.components.DataInfoPanel;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class DataPane extends JPanel {
+    DataInfoPanel dataInfoPanel;
+
+    public DataPane() {
+        for (int i = 0; i < 3; i++) {
+            dataInfoPanel = new DataInfoPanel();
+            this.add(dataInfoPanel);
+        }
+    }
 
 }
